1cf76f40d43fd8766c39d1407ff3f0982d87ab17
Hugos68 <hugokorteapple@gmail.com>
2023-02-08 15:54:02 +0100

    Major chess board improvements

--- src/lib/components/ChessBoard.svelte
+++ src/lib/components/ChessBoard.svelte
@@ -4,95 +4,34 @@
     const chess = new Chess(fenState);
     const board = chess.board();
     console.log(board);
+
+    const chessTypeIcon: Record<string, string> = {
+        'r': '♜',
+        'n': '♞',
+        'b': '♝',
+        'q': '♛',
+        'k': '♚',
+        'p': '♟'    
+    }
 </script>
 
-<table class="card w-[min(75vw,35rem)] aspect-square mx-auto border-collapse">
-    <tbody>
-        <tr>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-        </tr>
-        <tr>
-
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-        </tr>
-        <tr>
-    
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-        </tr>
-        <tr>
-     
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-        </tr>
-        <tr>
-   
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-        </tr>
-        <tr>
-  
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-        </tr>
-        <tr>
-     
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-        </tr>
-        <tr>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
-            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
-        </tr>
-    </tbody>
-</table>
+<div class="mx-auto flex flex-col justify-center items-center gap-4">
+    <h1>Chess Board</h1>
+    <table class="card w-[min(75vw,35rem)] aspect-square">
+        <tbody>
+            {#each board as rank, i} 
+                <tr>
+                    {#each rank as tile, j} 
+                        <td class="bg-[var(--chess-{i%2 === 0 ? (j%2===0 ? 'light' : 'dark') : (j%2===0 ? 'dark' : 'light')}-tile-color)] relative">
+                            {#if tile !==null}
+                                <p class="text-center !text-[min(7vw,3rem)]  {tile.color==='w' ? "text-base-token" : "text-dark-token"} absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+                                    {chessTypeIcon[tile.type]}
+                                </p>
+                            {/if}
+                        </td>
+                    {/each}
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
