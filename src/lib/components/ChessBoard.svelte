9652e86f5551e0cd8b4e41e965975f51ada0c193
hugos68 <hugokorteapple@gmail.com>
2023-02-08 20:55:10 +0100

    More chess

--- src/lib/components/ChessBoard.svelte
+++ src/lib/components/ChessBoard.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import { Chess } from 'chess.js'
+	import { onMount } from 'svelte';
     export let fenState: string;
+    export let sidePlaying: string;
     const chess = new Chess(fenState);
     
     const getTileColor = (rowNumber: number, tileNumber: number): string => {
@@ -11,24 +13,28 @@
             return tileNumber%2===0 ? "bg-[var(--chess-dark-tile-color)]" : "bg-[var(--chess-light-tile-color)]";
         }
     }
+
+    let flipped: boolean = false;
+    onMount(() => {
+        if (sidePlaying==="black") flipped = !flipped;
+    })
     
 </script>
 
-<div class="mx-auto flex flex-col justify-center items-center gap-4">
-    <h1>Chess Board</h1>
-    <table class="w-[min(75vw,35rem)] aspect-square">
-        <tbody>
-            {#each chess.board() as rank, i} 
-                <tr>
-                    {#each rank as tile, j} 
-                        <td class="{getTileColor(i, j)} relative">
-                            {#if tile !==null}
-                                <img class="z-[1] top-0 absolute" src="https://www.chess.com/chess-themes/pieces/neo/150/{tile.color}{tile.type}.png">
-                            {/if}
-                        </td>
-                    {/each}
-                </tr>
-            {/each}
-        </tbody>
-    </table>
-</div>
+
+<table class="w-[min(75vw,35rem)] aspect-square chess-board" class:rotate-180={flipped}>
+    <tbody>
+        {#each chess.board() as rank, i} 
+            <tr>
+                {#each rank as tileInfo, j} 
+                    <td class="{getTileColor(i, j)} relative chess-board-square">
+                        {#if tileInfo !==null}
+                            <img class="z-[1] top-0 absolute" class:rotate-180={flipped} src="https://www.chess.com/chess-themes/pieces/neo/150/{tileInfo.color}{tileInfo.type}.png" alt="{tileInfo.color}{tileInfo.type}">
+                        {/if}
+                    </td>
+                {/each}
+            </tr>
+        {/each}
+    </tbody>
+</table>
+
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
     import ChessBoard from "$lib/components/ChessBoard.svelte";
+    import { supabase } from "$lib/supabase";
+    // TODO Get chess game and subscribe to it
 </script>
 
-<ChessBoard fenState="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" />
+<div class="mx-auto flex flex-col justify-center items-center gap-4">
+    <ChessBoard sidePlaying="black" fenState="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" />
+</div>
