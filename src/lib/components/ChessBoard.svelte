750f7f3546eeebcb2d836e55912949de47ea1356
Hugos68 <hugokorteapple@gmail.com>
2023-02-08 14:54:23 +0100

    Added chessboard and providers

---
+++ src/lib/components/ChessBoard.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+    import { Chess } from 'chess.js'
+    export let fenState: string;
+    const chess = new Chess(fenState);
+    const board = chess.board();
+    console.log(board);
+</script>
+
+<table class="card w-[min(75vw,35rem)] aspect-square mx-auto border-collapse">
+    <tbody>
+        <tr>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+        </tr>
+        <tr>
+
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+        </tr>
+        <tr>
+    
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+        </tr>
+        <tr>
+     
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+        </tr>
+        <tr>
+   
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+        </tr>
+        <tr>
+  
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+        </tr>
+        <tr>
+     
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+        </tr>
+        <tr>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-dark-color)] relative"></td>
+            <td class="w-[12.5%] aspect-square bg-[var(--chess-light-tile-color)] relative"></td>
+        </tr>
+    </tbody>
+</table>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import ChessBoard from "$lib/components/ChessBoard.svelte";
+</script>
+
+<ChessBoard fenState="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" />
