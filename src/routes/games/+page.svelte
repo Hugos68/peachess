cb1597d3acbebb215eb346356a8e5fbc654369e0
Hugos68 <hugokorteapple@gmail.com>
2023-02-09 17:21:39 +0100

    Removed giant chessboard from homepage

--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -13,11 +13,10 @@
 <h1>Browse games</h1>
 <hr class="my-4" />
 {#each chessRecords as chessRecord}
-<div class="card p-4 pt-10 w-[20rem]">
-    <p>Player White: {chessRecord.player_id_white}</p>
-    <p>VS</p>
-    <p>Player Black: {chessRecord.player_id_black}</p>
-    <ChessBoard chess={new Chess(chessRecord.fen)} flipped={false} />
-</div>
-
+    <div class="card p-4 pt-10 w-[20rem]">
+        <p>Player White: {chessRecord.player_id_white}</p>
+        <p>VS</p>
+        <p>Player Black: {chessRecord.player_id_black}</p>
+        <ChessBoard chess={new Chess(chessRecord.fen)} flipped={false} />
+    </div>
 {/each}
