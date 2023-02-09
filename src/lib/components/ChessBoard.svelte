24eb78b2348dcc09f7452e8fa80d5a06a285dcba
Hugos68 <hugokorteapple@gmail.com>
2023-02-09 08:17:18 +0100

    Board flipping is in place

--- src/lib/components/ChessBoard.svelte
+++ src/lib/components/ChessBoard.svelte
@@ -10,17 +10,18 @@
 
     const getTileColor = (rowNumber: number, tileNumber: number): string => {
         if (rowNumber%2 === 0) {
-            return tileNumber%2===0 ? "bg-[var(--chess-light-tile-color)]" : "bg-[var(--chess-dark-tile-color)]";
+            return tileNumber%2 === 0 ? "bg-[var(--chess-light-tile-color)]" : "bg-[var(--chess-dark-tile-color)]";
         }
         else {
-            return tileNumber%2===0 ? "bg-[var(--chess-dark-tile-color)]" : "bg-[var(--chess-light-tile-color)]";
+            return tileNumber%2 === 0 ? "bg-[var(--chess-dark-tile-color)]" : "bg-[var(--chess-light-tile-color)]";
         }
     }
 
-    let flipped: boolean = false;
-    onMount(() => {
-        if (playingColor==='b') flipped = !flipped;
-    });
+    let flipped: boolean = playingColor==='b';
+
+    const getArrayOrder = (array: any[]) => {
+        return flipped ? array.reverse() : array;
+    }
 </script>
 
 {#if playingColor===turnColor}
@@ -28,19 +29,37 @@
 {:else}
     <p>Waiting for {playingColor === 'w' ? "Black" : "White"} to play</p>
 {/if}
-<table class="w-[min(75vw,35rem)] aspect-square" class:rotate-180={flipped}>
+
+<button class="btn variant-filled-primary" on:click={() => flipped = !flipped}>Flip Board</button>
+<table class="w-[min(75vw,35rem)] aspect-square">
     <tbody>
-        {#each chess.board() as rank, i} 
-            <tr>
-                {#each rank as tileInfo, j} 
-                    <td class="{getTileColor(i, j)} relative">
-                        {#if tileInfo !==null}
-                            <img class="z-[1] top-0 absolute" class:rotate-180={flipped} src="https://www.chess.com/chess-themes/pieces/neo/150/{tileInfo.color}{tileInfo.type}.png" alt="{tileInfo.color}{tileInfo.type}">
-                        {/if}
-                    </td>
-                {/each}
-            </tr>
-        {/each}
+        {#key flipped}
+            {#each getArrayOrder(chess.board()) as rank, i} 
+                <tr>
+                    {#each getArrayOrder(rank) as tileInfo, j} 
+                        <td class="{getTileColor(i, j)} relative">
+                            {#if tileInfo !==null}
+                                <img class="z-[1] top-0 absolute" src="https://www.chess.com/chess-themes/pieces/neo/150/{tileInfo.color}{tileInfo.type}.png" alt="{tileInfo.color}{tileInfo.type}">
+                            {/if}
+                            {#if j===0}
+                                {#if flipped}
+                                    <p class="absolute top-1 left-1">{i+1}</p>
+                                {:else}
+                                    <p class="absolute top-1 left-1">{8-i}</p>
+                                {/if}
+                            {/if}
+                            {#if i===7}
+                                {#if flipped}
+                                    <p class="absolute bottom-1 right-1">{8-j}</p>
+                                {:else}
+                                    <p class="absolute bottom-1 right-1">{j+1}</p>
+                                {/if}
+                            {/if}
+                        </td>
+                    {/each}
+                </tr>
+            {/each}
+        {/key}
     </tbody>
 </table>
 
