af4605cee54fc12a1ce6ba420926831880b974ca
Hugos68 <hugokorteapple@gmail.com>
2023-02-13 21:27:57 +0100

    cleanup

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -10,7 +10,6 @@ export const actions: Actions = {
 
         if (body.password!==body.confirmPassword) return fail(400, { message: 'Password mismatch' });
          
-        
         if (!body.displayName || !body.email || !body.password) return fail(400, { message: 'Please fill in all fields' });
         
         const {supabaseClient} = await getSupabase(event);
