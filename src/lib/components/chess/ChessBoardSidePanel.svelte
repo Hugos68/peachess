c0ca792774256f5e750d0f655a5d61cfcbe6c23d
Hugos68 <hugokorteapple@gmail.com>
2023-02-20 11:06:29 +0100

    Styling

--- src/lib/components/chess/ChessBoardSidePanel.svelte
+++ src/lib/components/chess/ChessBoardSidePanel.svelte
@@ -18,7 +18,7 @@
 	}
 </script>
 
-<TabGroup regionPanel="flex-1 flex flex-col overflow-hidden" class="h-full w-full card !bg-secondary-700 p-4 flex flex-col">
+<TabGroup regionPanel="flex-1 flex flex-col overflow-hidden" class="h-full w-full card !bg-surface-100-800-token p-4 flex flex-col">
     <Tab bind:group={tabSet} name="moves" value={0}>Moves</Tab>
     <Tab bind:group={tabSet} name="chat" value={1}>Chat</Tab>
     <Tab bind:group={tabSet} name="settings" value={2}>Settings</Tab>
--- src/lib/stores/settings-store.ts
+++ src/lib/stores/settings-store.ts
@@ -1,8 +1,9 @@
 import { localStorageStore } from "@skeletonlabs/skeleton";
 
+// All values below are defaults
 export const settings: Writable<Settings> = localStorageStore('settings',  {
     animate: true,
-    animationDuration: 100,
+    animationDuration: 250,
     sfx: true,
     premove: false,
     drag: true,
