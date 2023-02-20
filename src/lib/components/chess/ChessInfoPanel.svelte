146b194213e4c521a586b1c9bb75c9b6cf93a589
hugos68 <hugokorteapple@gmail.com>
2023-02-21 00:04:37 +0100

    change no. to move in the panel tab

--- src/lib/components/chess/ChessInfoPanel.svelte
+++ src/lib/components/chess/ChessInfoPanel.svelte
@@ -41,7 +41,7 @@
         {#if tabSet === 0}
         {@const activeMoveIndex = $chessStateStore.moveStack.length-1}
             <ul class="overflow-y-scroll flex-1 grid grid-cols-3 place-items-center" id="move-list">
-                <span class="p-1"><strong>No.</strong></span>
+                <span class="p-1"><strong>Move</strong></span>
                 <span class="p-1"><strong>White</strong></span>
                 <span class="p-1"><strong>Black</strong></span>
                 {#each $chessStateStore.moveStack.concat($chessStateStore.undoneMoveStack.slice().reverse()) as move, i} 
