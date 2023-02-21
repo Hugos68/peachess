31027d962d7b9d8054f86d57c947a39c1d526d19
Hugos68 <hugokorteapple@gmail.com>
2023-02-21 11:33:19 +0100

    Migrated to the chessboard component

--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import { page } from "$app/stores";
+	import OnlineChessBoard from "$lib/components/chess/OnlineChessBoard.svelte";
 	import NewGameModal from "$lib/components/modal/NewGameModal.svelte";
-	import { supabase } from "$lib/supabase";
+	import { createChessStateStore, type ChessStateStore } from "$lib/stores/chess-store";
 	import { modalStore, toastStore, type ModalComponent, type ModalSettings, type ToastSettings } from "@skeletonlabs/skeleton";
-	import type { RealtimeChannel } from "@supabase/supabase-js";
-	import { Chess } from "chess.js";
-	import { Chessground } from "chessground";
-	import { onDestroy, onMount } from "svelte";
+	import { onMount } from "svelte";
+	import { get } from "svelte/store";
 	import type { PageData } from "./$types";
 
     export let data: PageData;
@@ -21,44 +20,11 @@
     } 
 
     let mounted: boolean = false;
-    const channels: RealtimeChannel[] = [];
-    let chessGameChessObjectMap: Map<number, Chess> = new Map();
+    const chessStateStores: ChessStateStore[] = [];
     onMount(() => {
-        data.chessGames.forEach(chessGame => {
-            const chess = new Chess();
-            chess.loadPgn(chessGame.pgn);
-
-            const chessBoard = Chessground(document.getElementById(`board${chessGame.id}`) as HTMLElement, {
-                fen: chess.fen(),
-                viewOnly: true
-            });
-            chessGameChessObjectMap.set(chessGame.id, chess);
-
-            // Only open a channel when the game is ongoing 
-            if (!chess.isGameOver()) {
-                const channel = supabase
-                .channel('table-db-changes')
-                .on(
-                    'postgres_changes',
-                    {
-                        event: 'UPDATE',
-                        schema: 'public',
-                        table: 'games',
-                        filter: `id=eq.${chessGame.id}`
-                    },
-                    // This callback is called whenever this game gets an update, payload contains the old and new version
-                    (payload) => {
-                        const updatedChessGame: ChessGame = payload.new as ChessGame;
-                        chess.loadPgn(updatedChessGame.pgn);
-                        chessBoard.set({
-                            fen: chess.fen(),
-                            viewOnly: true
-                        });
-                    }
-                )
-                .subscribe();
-                channels.push(channel);
-            }
+        data.chessGames.forEach(chessGame =>  {
+            const store = createChessStateStore(chessGame);
+            chessStateStores.push(store);
         });
         mounted = true;
     });
@@ -74,12 +40,6 @@
         };
         modalStore.trigger(modal);
     }
-
-    onDestroy(() => {
-        channels.forEach(channel => {
-            channel.unsubscribe();
-        })
-    });
 </script>
 
 <div class="mt-[5vh] flex flex-col gap-8">
@@ -90,24 +50,30 @@
 
     {#if !mounted}
         <p class="mx-auto my-auto mt-[15vh]">Loading boards...</p>
-    {/if}
+    {:else}
     <div class="flex flex-wrap gap-8" class:hidden={!mounted}>
-        {#each data.chessGames as chessGame} 
-            <a class="card h-full w-full flex-[25rem] p-4 flex flex-col group" href="/games/{chessGame.id}">
-                {#if mounted} 
-                <div class="pb-4 flex justify-between">
-                    <p class="font-bold">Game {chessGame.id}: {chessGameChessObjectMap.get(chessGame.id)?.header()['White']} vs {chessGameChessObjectMap.get(chessGame.id)?.header()['Black']}</p>
-                    {#if !chessGameChessObjectMap.get(chessGame.id)?.isGameOver()}
+        
+        {#each chessStateStores as chessStateStore}     
+            {@const chessState = get(chessStateStore)}
+            <a class="card h-full w-full flex-[25rem] p-4 gap-2 flex flex-col group" href="/games/{chessState.chessGame.id}">
+      
+                <div class="flex justify-between">
+                    <p><strong>Game {chessState.chessGame.id}: {chessState.chess.header()['White']} vs {chessState.chess.header()['Black']}</strong></p>
+                    {#if !chessState.chess.isGameOver()}
                         <div class="badge bg-red-400">
                             <div class="w-2 aspect-square rounded-full bg-white"></div>
                             <span class="font-bold">Live</span>
                         </div>
                     {/if}
                 </div>
-                {/if}
-                <div class="group-hover:brightness-75 transition-[filter] duration-250" id="board{chessGame.id}"></div>
+         
+                <div class="group-hover:brightness-75 transition-[filter] duration-250">
+                    <OnlineChessBoard chessStateStore={chessStateStore} />
+                </div>
             </a>
         {/each}
         <div class="flex-[1_0_25rem] p-4 h-0"></div>
     </div>
+    {/if}
+
 </div>
