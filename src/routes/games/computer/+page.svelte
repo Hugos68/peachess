c51f70026e1c69fe9dde89080f6c5c25e1ec54fb
hugos68 <hugokorteapple@gmail.com>
2023-02-24 00:07:59 +0100

    Mobile overhaul

--- src/routes/games/computer/+page.svelte
+++ src/routes/games/computer/+page.svelte
@@ -7,10 +7,28 @@
 	import ChessBoardSidePanel from "$lib/components/chess/ChessInfoPanel.svelte";
 	import type { Writable } from "svelte/store";
 	import ChessBoard from "$lib/components/chess/ChessBoard.svelte";
+	import { modalStore, type ModalComponent, type ModalSettings } from "@skeletonlabs/skeleton";
+	import ChessInfoPanel from "$lib/components/chess/ChessInfoPanel.svelte";
     
     export let data: PageData;
 
     const chessStateStore: Writable<AIChessState> = createAIChessStateStore(data.difficulity as 0 | 1 | 2 | 3 | 4, 'w');
+
+        const openGamePanel = () => {
+        const modalComponent: ModalComponent = {
+		    ref: ChessInfoPanel,
+            props: { 
+                chessStateStore,
+                width: "w-full",
+                height: "h-[66vh]"
+            },
+        };
+	    const modal: ModalSettings = {
+            type: 'component',
+            component: modalComponent,
+        };
+        modalStore.trigger(modal);
+    }
 </script>
 
  <div class="mx-auto xl:h-[calc(100vh-2rem)] flex flex-col xl:flex-row justify-center items-center gap-12">
@@ -69,7 +87,9 @@
         </footer>
     </div>
 
-    <div class="h-[min(calc(100vw)-1rem,calc(95vh-12rem))] w-[min(calc(100vw)-1rem,calc(95vh-12rem))]">
-        <ChessBoardSidePanel chessStateStore={chessStateStore} />
+    <div class="hidden xl:block">
+        <ChessBoardSidePanel height="h-[min(calc(100vw)-1rem,calc(95vh-12rem))]" width="w-[min(calc(100vw)-1rem,calc(95vh-12rem))]" chessStateStore={chessStateStore} />
     </div>
+
+    <button class="btn variant-filled-primary xl:hidden" on:click={openGamePanel}>Open Game Panel</button>
  </div>
