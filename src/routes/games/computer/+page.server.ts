13e616970f75cb2048434f8ee9b6fccf907bf2d6
Hugos68 <hugokorteapple@gmail.com>
2023-02-23 13:10:35 +0100

    You can now have different difficulity's

--- src/routes/games/computer/+page.server.ts
+++ src/routes/games/computer/+page.server.ts
@@ -1,7 +1,8 @@
 export const load: PageServerLoad = async ({url}) => {
     let difficulity = url.searchParams.get('difficulity');
     if (!difficulity) difficulity = 0;
-    if (typeof difficulity !== typeof 0 | 1 | 2 | 3 | 4) difficulity = 0;
+    difficulity = Number.parseInt(difficulity);
+    if (difficulity < 0 || difficulity > 4) difficulity = 0;
     return {
         difficulity
     }
