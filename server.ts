e3bcb4d4a052f34b2f799bc9beb17bff84cfacfa
hugos68 <hugokorteapple@gmail.com>
2023-03-03 21:30:02 +0100

    Added custom node server, removed cors

---
+++ server.ts
@@ -0,0 +1,17 @@
+import { handler } from './build/handler.js';
+import express from 'express';
+ 
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    next();
+});
+ 
+// let SvelteKit handle everything else, including serving prerendered pages and static assets
+app.use(handler);
+ 
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,10 +2,6 @@ import type { LayoutServerLoad } from './$types'
 import { getServerSession } from '@supabase/auth-helpers-sveltekit'
 
 export const load: LayoutServerLoad = async (event) => {
-  // event.setHeaders( {
-  //   'Cross-Origin-Embedder-Policy': 'require-corp',
-  //   'Cross-Origin-Opener-Policy': 'same-origin'
-  // });
   return {
     session: await getServerSession(event),
   }
