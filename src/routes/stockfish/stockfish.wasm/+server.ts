e0025e79d3fd354688cb287c1188b0e72499e16f
hugos68 <hugokorteapple@gmail.com>
2023-03-06 00:00:04 +0100

    FINALLY GOT STOCKFISH TO WORK WITHOUT VITE SERVER CONFIG

--- src/routes/stockfish/stockfish.wasm/+server.ts
+++ src/routes/stockfish/stockfish.wasm/+server.ts
@@ -1,8 +1,8 @@
 import type { RequestHandler } from './$types';
-import file from './stockfish.wasm?raw';
+import * as fs from 'fs';
 
 export const GET: RequestHandler = async () => {
     const headers = new Headers();
     headers.append('Content-Type', 'application/wasm');
-    return new Response(file, { headers, status: 200 });
+    return new Response(fs.readFileSync('./src/routes/stockfish/stockfish.wasm/stockfish.wasm'), { headers, status: 200 });
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-const stockfishHeaderMiddleware = {
-	name: 'handle-stockfish-wasm-headers',
-		configureServer(server) {
-		server.middlewares.use((req, res, next) => {
-			res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-			res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-			next();
-		});
-	}
-  };
-
 export default defineConfig({
 	plugins: [sveltekit()],
 });
+	
