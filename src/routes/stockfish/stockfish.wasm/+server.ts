dd5630fdbf5f032aa230d1bd913cd4218b7ce449
hugos68 <hugokorteapple@gmail.com>
2023-03-06 00:05:41 +0100

    Tried to fix path

--- src/routes/stockfish/stockfish.wasm/+server.ts
+++ src/routes/stockfish/stockfish.wasm/+server.ts
@@ -1,8 +1,8 @@
 import type { RequestHandler } from './$types';
-import * as fs from 'fs';
+import fs from 'fs';
 
 export const GET: RequestHandler = async () => {
     const headers = new Headers();
     headers.append('Content-Type', 'application/wasm');
-    return new Response(fs.readFileSync('./src/routes/stockfish/stockfish.wasm/stockfish.wasm'), { headers, status: 200 });
+    return new Response(fs.readFileSync('src/routes/stockfish/stockfish.wasm/stockfish.wasm'), { headers, status: 200 });
 };
