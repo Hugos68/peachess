e08e0ae25395319cf420c04503f7231b39c4f797
Hugos68 <hugokorteapple@gmail.com>
2023-03-06 09:48:19 +0100

    Trying to get prod paths using readdirsync

--- src/routes/stockfish/stockfish.js/+server.ts
+++ src/routes/stockfish/stockfish.js/+server.ts
@@ -1,8 +1,11 @@
 import type { RequestHandler } from './$types';
-import fs from 'fs';
+import fs from 'node:fs';
+import { dev } from '$app/environment'; 
 
 export const GET: RequestHandler = async () => {
+    console.log(fs.readdirSync('.'));
     const headers = new Headers();
     headers.append('Content-Type', 'text/javascript');
-    return new Response(fs.readFileSync('/stockfish-files/stockfish.js'), { headers, status: 200 });
+    if (dev) return new Response(fs.readFileSync('./static/stockfish-files/stockfish.js'), { headers, status: 200 });
+    else return new Response(fs.readFileSync('/stockfish-files/stockfish.js'), { headers, status: 200 });
 };
--- src/routes/stockfish/stockfish.worker.js/+server.ts
+++ src/routes/stockfish/stockfish.worker.js/+server.ts
@@ -1,8 +1,10 @@
 import type { RequestHandler } from './$types';
 import fs from 'fs';
+import { dev } from '$app/environment'; 
 
 export const GET: RequestHandler = async () => {
     const headers = new Headers();
     headers.append('Content-Type', 'text/javascript');
-    return new Response(fs.readFileSync('/stockfish-files/stockfish.worker.js'), { headers, status: 200 });
+    if (dev) return new Response(fs.readFileSync('./static/stockfish-files/stockfish.worker.js'), { headers, status: 200 });
+    else return new Response(fs.readFileSync('/stockfish-files/stockfish.worker.js'), { headers, status: 200 });
 };
