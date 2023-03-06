95d10df2df038ef3c4f70e19e8b758ba1a7a86ec
hugos68 <hugokorteapple@gmail.com>
2023-03-06 01:14:32 +0100

    Hopefully it works in prod

--- src/routes/stockfish/stockfish.wasm/+server.ts
+++ src/routes/stockfish/stockfish.wasm/+server.ts
@@ -4,5 +4,5 @@ import fs from 'fs';
 export const GET: RequestHandler = async () => {
     const headers = new Headers();
     headers.append('Content-Type', 'application/wasm');
-    return new Response(fs.readFileSync('/static/stockfish-files/stockfish.wasm'), { headers, status: 200 });
+    return new Response(fs.readFileSync('/stockfish-files/stockfish.wasm'), { headers, status: 200 });
 };
